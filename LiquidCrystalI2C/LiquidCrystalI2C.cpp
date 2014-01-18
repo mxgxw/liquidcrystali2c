640f1c49fd235405eae7ff1bca9d113ac4c8a7eb
Mario Gomez <mario.gomez@teubi.co>
2014-01-18 10:53:25 -0600

    Fixed typos on copyright notices.

--- LiquidCrystalI2C/LiquidCrystalI2C.cpp
+++ LiquidCrystalI2C/LiquidCrystalI2C.cpp
@@ -1,13 +1,13 @@
 /**************************************************************************
  * LiquidCrystal-I2C a simple way to control a 16x2 LCD display over I2C. *
- * Copyright (C) 2014 Mario Gomez (info -at- teubi.co).                                       *
+ * Copyright (C) 2014 Mario Gomez (info -at- teubi.co).                   *
  *                                                                        *
  * This program is free software; you can redistribute it and/or modify   *
  * it under the terms of the GNU General Public License as published by   *
  * the Free Software Foundation; either version 3 of the License, or      *
  * any later version.                                                     *
  *                                                                        *
- * This rogram is distributed in the hope that it will be useful,         *
+ * This program is distributed in the hope that it will be useful,        *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of         *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the           *
  * GNU General Public License for more details.                           *
